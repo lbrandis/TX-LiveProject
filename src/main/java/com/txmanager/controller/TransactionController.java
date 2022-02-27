972256e06ab544be95c960ebc982f20f934b3060
Leonid Brandis <leonid.brandis@mac.com>
2022-02-26 20:23:23 -0500

    remove unused imports

--- src/main/java/com/txmanager/controller/TransactionController.java
+++ src/main/java/com/txmanager/controller/TransactionController.java
@@ -3,7 +3,6 @@ package com.txmanager.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.converter.json.GsonBuilderUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
