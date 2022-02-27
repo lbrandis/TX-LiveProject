8255f22631983392a155da3be3fbe7c0f731981b
Leonid Brandis <leonid.brandis@mac.com>
2022-02-26 20:22:39 -0500

    Remove unused imports

--- src/test/java/com/txmanager/service/TransactionServiceTest.java
+++ src/test/java/com/txmanager/service/TransactionServiceTest.java
@@ -1,7 +1,6 @@
 package com.txmanager.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
